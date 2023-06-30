e47c56743672b46ab3261e212f67b4a5b1461daa
Vethanathan <58129139+Vethanathan@users.noreply.github.com>
2023-06-30 14:40:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import discord
 import responses
+import os
+key = 'TOKEN'
+
 
 
 async def send_message(message, user_message, is_private):
@@ -38,4 +41,4 @@ def run_discord_bot():
         else:
             await send_message(message, user_message, is_private=False)
 
-    client.run(TOKEN)
+    client.run(os.getenv(key))
